fd7d67d4ae2716600de989f36054b5b271954ce6
Priyanka Dhakal <30001935@student.toiohomai.ac.nz>
2019-03-20 16:42:29 +1300

    exercises of umi

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        class Person
+        {
+            public string Fname { get; set; }
+            public string lname { get; set; }
+        }
+        class Student : Person
+        {
+            public int Studentid { get; set; }
+            public Student(string _fname, string _lname, string _stdid)
+
+            Fname = _fname;
+            Lname = _lname;
+
+
+          
+
+
+
+
+
+
+        }
+
+
+    }
+}
